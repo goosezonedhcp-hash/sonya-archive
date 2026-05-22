f941fed91ac78d72550f92db6f0183e11f36ad1c
goosezonedhcp-hash <goosezonedhcp@gmail.com>
2026-05-22 22:12:11 +0300

    HOT FIX app.js

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const app = document.getElementById("app");
 
 app.innerHTML = `
+
 <div class="topbar">
-    <span>SONYA ARCHIVE // OBSERVATION INTERFACE</span>
-    <span>NIGHTFALL-7</span>
-    <span id="clock"></span>
+
+<span>
+SONYA ARCHIVE //
+OBSERVATION INTERFACE
+</span>
+
+<span>
+NIGHTFALL-7
+</span>
+
+<span id="clock"></span>
+
 </div>
+
+
 
 <div class="layout">
 
 <aside class="sidebar panel">
 
 <h2>SONYA ARCHIVE</h2>
-<div class="ver">v2.0 // CASE FILES</div>
+
+<div class="ver">
+
+v2.0 //
+CASE FILES
+
+</div>
 
 <div class="section">
+
 НАВИГАЦИЯ
 
 <ul>
+
 <li>⌂ ГЛАВНАЯ</li>
 <li>⌘ ДОСЬЕ: СОНЯ</li>
 <li>✢ АРХИВ ДЕЛ</li>
@@ -25,19 +45,21 @@ app.innerHTML = `
 <li>📝 ЗАМЕТКИ</li>
 <li>◫ МЕДИА</li>
 <li>⚙ СИСТЕМА</li>
+
 </ul>
 
 </div>
 
+
 <div class="section">
 
-СТАТУС АРХИВА<br>
 АРХИВ: АКТИВЕН<br>
 НАБЛЮДЕНИЕ: ВКЛ<br>
 СУБЪЕКТ: СОНЯ<br>
 ПРОТОКОЛ: NIGHTFALL
 
 </div>
+
 
 <div class="quote">
 
@@ -51,6 +73,7 @@ app.innerHTML = `
 </aside>
 
 
+
 <main>
 
 <section class="panel hero">
@@ -58,10 +81,10 @@ app.innerHTML = `
 <div class="portrait">
 
 <img
-src="assets/sonya_portrait.png"
-alt="sonya">
+src="assets/sonya_portrait.png">
 
 </div>
+
 
 <div class="heroInfo">
 
@@ -73,29 +96,28 @@ alt="sonya">
 
 СТАТУС [ONLINE]<br>
 СОН [НЕСТАБИЛЕН]<br>
-РАСПОЛОЖЕНИЕ: КВАРТИРА 28<br><br>
+КВАРТИРА 28<br><br>
 
-УРОВЕНЬ ОПАСНОСТИ:
+УРОВЕНЬ ОПАСНОСТИ<br>
 
 ⬛⬛⬛⬜⬜
 
-<br><br>
-
-Субъект представляет интерес.<br>
-Рекомендуется продолжать наблюдение.
-
 </div>
+
 
 <div class="seal">
 
-<img src="assets/seal_eye.png">
+<img
+src="assets/seal_eye.png">
 
 </div>
 
 </section>
 
 
+
 <div class="grid">
+
 
 <section class="panel case">
 
@@ -110,14 +132,8 @@ src="assets/case001_door28.png">
 
 КВАРТИРА 28<br><br>
 
-ОБЪЕКТ:
-неопознанная сущность<br><br>
-
-ТИП:
-искажённая реальность<br><br>
-
-УГРОЗА:
-⬛⬛⬛⬜⬜
+НЕОПОЗНАННАЯ
+СУЩНОСТЬ
 
 </div>
 
@@ -129,7 +145,12 @@ src="assets/case001_door28.png">
 
 <section class="panel case">
 
-<h3>ДЕЛО №002 «МОРФЕЙ»</h3>
+<h3>
+
+ДЕЛО №002
+«МОРФЕЙ»
+
+</h3>
 
 <div class="caseWrap">
 
@@ -137,11 +158,6 @@ src="assets/case001_door28.png">
 src="assets/observer_eye.png">
 
 <div>
-
-ПСИХОАКТИВНОЕ
-ВОЗДЕЙСТВИЕ<br><br>
-
-СТАТУС:<br>
 
 🔒 ДОСТУП
 ОГРАНИЧЕН
@@ -156,7 +172,12 @@ src="assets/observer_eye.png">
 
 <section class="panel observer">
 
-<h3>ИЗМЕНЕНИЯ СУБЪЕКТА</h3>
+<h3>
+
+ИЗМЕНЕНИЯ
+СУБЪЕКТА
+
+</h3>
 
 <div class="observerWrap">
 
@@ -165,10 +186,9 @@ src="assets/paper_note.png">
 
 <div>
 
-+ синяк под глазом<br>
 + нарушение сна<br>
-+ повышенная усталость<br>
-+ неизвестная реакция
++ усталость<br>
++ реакция
 
 </div>
 
@@ -182,8 +202,8 @@ src="assets/paper_note.png">
 
 <h3>
 
-ЛИЧНОЕ ПРОСТРАНСТВО
-// ПОСЛЕДНИЙ КАДР
+ПОСЛЕДНИЙ
+КАДР
 
 </h3>
 
@@ -195,14 +215,14 @@ src="assets/room_lastframe.png">
 </div>
 
 
+
 <section class="panel protocol">
 
-СИСТЕМНЫЙ ПРОТОКОЛ<br><br>
+[INFO]
+соединение установлено<br>
 
-[INFO] соединение установлено<br>
-[WARN] обнаружена активность<br>
-[WARN] сектор квартира 28<br>
-[INFO] наблюдение продолжается
+[WARN]
+активность квартира 28
 
 </section>
 
@@ -210,21 +230,16 @@ src="assets/room_lastframe.png">
 
 
 
+
 <aside class="rightbar">
 
 <section class="panel">
 
-<h3>
-
-ПОСЛЕДНИЕ
-НАБЛЮДЕНИЯ // LOG
-
-</h3>
+<h3>LOG</h3>
 
 02:14 субъект вернулся<br>
-02:16 долго стояла у окна<br>
-02:29 активность высокая<br>
-02:33 смотрит на экран
+02:16 окно<br>
+02:33 смотрит
 
 </section>
 
@@ -239,7 +254,12 @@ src="assets/paper_note.png">
 
 <section class="panel">
 
-<h3>КАРТА ГОРОДА</h3>
+<h3>
+
+КАРТА
+ГОРОДА
+
+</h3>
 
 <img
 src="assets/city_map.png">
@@ -249,74 +269,29 @@ src="assets/city_map.png">
 </aside>
 
 </div>
-`;
 
-setInterval(()=>{
-
-document.getElementById(
-"clock"
-
-).innerText=
-
-new Date()
-.toLocaleTimeString()
-
-},1000);
-
-+ нарушение сна<br>
-+ метка<br>
-+ неизвестная реакция
-
-</section>
-
-<section class="panel">
-<h3>ЛИЧНОЕ ПРОСТРАНСТВО</h3>
-
-комната сони<br>
-монитор • кружка<br>
-игрушки • плакаты
-
-</section>
-
-</div>
-
-<section class="panel">
-СИСТЕМНЫЙ ПРОТОКОЛ<br>
-[WARN] аномалия сектор Квартира 28
-</section>
-
-</main>
-
-<aside>
-
-<section class="panel">
-LOG<br><br>
-
-02:33 смотрит на экран<br>
-02:44 сигнал стабилен
-
-</section>
-
-<section class="panel warn">
-ПОМНИ:<br><br>
-
-наблюдать, а не спасать.
-
-</section>
-
-</aside>
-
-</div>
 `;
 
 function updateClock(){
-document.getElementById(
+
+document
+.getElementById(
 "clock"
-).innerText=
-new Date().toLocaleTimeString(
+)
+
+.innerText=
+
+new Date()
+
+.toLocaleTimeString(
 "ru-RU"
 );
+
 }
 
-setInterval(updateClock,1000);
 updateClock();
+
+setInterval(
+updateClock,
+1000
+);
